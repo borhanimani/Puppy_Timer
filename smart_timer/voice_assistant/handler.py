448562695731f8ef80d32ae78858d9b7d3d11f70
Borhan <manibor99@gmail.com>
2026-08-22 13:49:49 +0330

    add handler for voice assistant

---
+++ smart_timer/voice_assistant/handler.py
@@ -0,0 +1,39 @@
+from voice_assistant.engine import VoiceEngine
+
+
+class VoiceAssistantHandler:
+    def __init__(self, command):
+        self._voice_engine = VoiceEngine(
+            "voice_assistant/models/vosk-model-small-en-us-0.15",
+            callback= command,
+        )
+
+    def run_engine(self):
+        try:
+
+            self._voice_engine.start()
+
+            print("Mic on")
+            print("Ctrl+C to exit")
+            print("Listening...")
+
+            while True:
+                pass
+
+        except KeyboardInterrupt:
+
+            print(
+                "\nCtrl+C received."
+            )
+
+        finally:
+
+            print(
+                "Shutting down..."
+            )
+
+            self._voice_engine.stop()
+
+            print(
+                "Voice engine stopped"
+            )
