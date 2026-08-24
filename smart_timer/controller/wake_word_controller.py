5821cfd11c3ee91ae2e9886beaf12753f64b82d8
Borhan <manibor99@gmail.com>
2026-08-24 13:00:02 +0330

    update controllers

--- smart_timer/controller/wake_word_controller.py
+++ smart_timer/controller/wake_word_controller.py
@@ -2,15 +2,20 @@ from voice_assistant.handler import VoiceAssistantHandler
 from controller.keywords import WAKE_WORDS
 
 class WakeWordController:
-    def __init__(self):
+    def __init__(self, on_wake_word):
         self._voice_assisstant_handler = None
+        self._on_wake_word = on_wake_word
 
     def check_word(self,text):
         words = text.lower().split(' ')
         for word in words:
             if word in WAKE_WORDS:
-                print("I'm")
-                self._voice_assisstant_handler.stop_engine()
+                self.stop_wake_word()
+
+                if self._on_wake_word:
+                    self._on_wake_word()
+
+                return
 
 
     def load_assistant(self):
@@ -18,4 +23,6 @@ class WakeWordController:
         self._voice_assisstant_handler.run_engine()
 
     def stop_wake_word(self):
-        self._voice_assisstant_handler.stop_engine()
+        if self._voice_assisstant_handler:
+            self._voice_assisstant_handler.stop_engine()
+            self._voice_assisstant_handler = None
