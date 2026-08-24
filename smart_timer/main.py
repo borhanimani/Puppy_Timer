17964c3e794dcdb0bb55bf16343f5461bd39c16a
Borhan <manibor99@gmail.com>
2026-08-24 12:59:29 +0330

    make main and test

--- smart_timer/main.py
+++ smart_timer/main.py
@@ -1,18 +1,41 @@
-from voice_assistant.handler import VoiceAssistantHandler
-from controller.timer_voice_contoller import TimerVoiceController
-from controller.wake_word_controller import WakeWordController
+import sys
+from pathlib import Path
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
+from ui.main_window import MainWindow
 
-# def command(text):
-#     print(
-#         "heard:",
-#         text
-#     )
+BASE_DIR = Path(__file__).resolve().parent
 
-# handler = VoiceAssistantHandler(command)
-# handler.run_engine()
+ICON_PATH = (
+    BASE_DIR
+    / "assets"
+    / "icons"
+    / "puppy.png"
+)
 
-a = TimerVoiceController()
-a.load_assistant()
 
-# b = WakeWordController()
-# b.load_assistant()
+def main():
+
+    app = QApplication(sys.argv)
+
+    app.setWindowIcon(
+        QIcon(
+            str(ICON_PATH)
+        )
+    )
+
+    with open("ui/style.qss", "r", encoding="utf-8") as file:
+        app.setStyleSheet(file.read())
+
+
+    window = MainWindow()
+
+    window.show()
+
+    sys.exit(
+        app.exec()
+    )
+
+
+if __name__ == "__main__":
+    main()
